ef2735f914761e4af14d91f6b3d71aca18e689e6
feiweiwei <feiweiwei@shrbank.com>
2017-07-31 21:43:39 +0800

    增加注释

--- src/main/java/com/monkey01/springbootstart/controller/UserController.java
+++ src/main/java/com/monkey01/springbootstart/controller/UserController.java
@@ -1,10 +1,7 @@
 package com.monkey01.springbootstart.controller;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * Created by feiweiwei on 17/7/31.
@@ -12,13 +9,25 @@ import org.springframework.web.bind.annotation.RestController;
 //@RestController注解能够使项目支持Rest
 @RestController
 @SpringBootApplication
+//表示该controller类下所有的方法都公用的一级上下文根
 @RequestMapping(value = "/springboot")
 public class UserController {
-
+    //这里使用@RequestMapping注解表示该方法对应的二级上下文路径
     @RequestMapping(value = "/getUserByGet", method = RequestMethod.GET)
     String getUserByGet(@RequestParam(value = "userName") String userName){
         return "Hello " + userName;
     }
 
+    //通过RequestMethod.POST表示请求需要时POST方式
+    @RequestMapping(value = "/getUserByPost", method = RequestMethod.POST)
+    String getUserByPost(@RequestParam(value = "userName") String userName){
+        return "Hello " + userName;
+    }
 
+    //在入参设置@RequestBody注解表示接收整个报文体，这里主要用在接收整个POST请求中的json报文体，
+    //目前主流的请求报文也都是JSON格式了，使用该注解就能够获取整个JSON报文体作为入参，使用JSON解析工具解析后获取具体参数
+    @RequestMapping(value = "/getUserByJson",method = RequestMethod.POST)
+    String getUserByJson(@RequestBody String data){
+        return "Json is " + data;
+    }
 }
